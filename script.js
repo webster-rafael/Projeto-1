ae2d658b81f218cb023d257f4a3094b84c4f2de1
Webster <webster.rafael2014@gmail.com>
2023-07-06 02:16:06 -0300

    projetinho

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    const img = document.querySelector("#profile img")
+    if (html.classList.contains('light')) {
+        img.setAttribute("src", "Fotos/avatar-light.png")
+    } else {
+        img.setAttribute("src", "Fotos/avatar.png")
+    }
+
+    }
