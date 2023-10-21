d742c6820a7077a0a5e59ff6d21c28229c17137e
Webster Dev <webster.rafael2014@gmail.com>
2023-10-21 17:07:21 -0300

    NEW

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ function toggleMode() {
     }
 
     }
+
+    function saudacaoDeAcordoComHora() {
+        var agora = new Date();
+        var hora = agora.getHours();
+        var saudacao = "Olá!";
+        var saudacaoDiv = document.getElementById("saudacao");
+    
+        if (hora >= 5 && hora < 12) {
+            saudacao = "Bom dia!";
+        } else if (hora >= 12 && hora < 18) {
+            saudacao = "Boa tarde!";
+        } else if (hora >= 18) {
+            saudacao = "Boa noite!";
+        } else {
+            saudacao = "Boa madrugada!";
+        }
+    
+        saudacaoDiv.textContent = saudacao;
+    }
+    
+    saudacaoDeAcordoComHora();
+    
+    
